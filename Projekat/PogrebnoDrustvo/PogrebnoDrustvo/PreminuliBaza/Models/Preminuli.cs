03f53094d29d6115f44bd18d016ed577641d56fa
Mirza Mušić <mmusic3@etf.unsa.ba>
2016-04-24 16:44:31 -0700

    DbContext Tabela Preminulih

---
+++ Projekat/PogrebnoDrustvo/PogrebnoDrustvo/PreminuliBaza/Models/Preminuli.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DbContext_Tabela.PreminuliBaza.Models
+{
+    public class Preminuli
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int barKodUmrlog { get; set; }
+        public string imeUmrlog { get; set; }
+        public string prezimeUmrlog { get; set; }
+        public DateTime datumRodjenja { get; set; }
+        public DateTime datumSmrti { get; set; }
+        public string pogrebnaLokacija { get; set; }
+    }
+}
