044a6f851971b63f7d0da2dea89d4b7f52e04525
Mirza Mušić <mmusic3@etf.unsa.ba>
2016-04-25 04:43:27 -0700

    ZahtjevDbContext tabela (Haris Silajdžić)

---
+++ Projekat/PogrebnoDrustvo/PogrebnoDrustvo/ZahtjevBaza/Models/DefaultPodaciZahtjeva.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbContext_Tabela.ZahtjevBaza.Models
+{
+    class DefaultPodaciZahtjeva
+    {
+        public static void Initialize(ZahtjevDbContext context)
+        {
+            if (!context.zahtjevi.Any())
+            {
+                context.zahtjevi.AddRange(
+                new Zahtjev()
+                {
+                    troskoviPogreba = 100,
+                    listaOpreme = new List<string> {"lopata","tabut", "ćefin"},
+                    mjesto = "Bare",
+                    angazovanjeSnimatelja = true,
+                    transportOzaloscenih = true,
+                    placeno = false,
+                    potvrdaZahtjeva = true,
+                    izradaSmrtovnice = false
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Projekat/PogrebnoDrustvo/PogrebnoDrustvo/ZahtjevBaza/Models/Zahtjev.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DbContext_Tabela.ZahtjevBaza.Models
+{
+    public class Zahtjev
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int  idZahtjeva{ get; set; }
+        public string mjesto { get; set; }  // treba izmijeniti da bude tip Mjesto a ne string
+        public string preminuli { get; set; } // također
+        public List<string> listaOpreme { get; set; } // list<Oprema>
+        public double troskoviPogreba{ get; set; }
+        public bool izradaSmrtovnice { get; set; }
+        public bool transportOzaloscenih { get; set; }
+        public bool angazovanjeSnimatelja { get; set; }
+        public bool potvrdaZahtjeva { get; set; }
+        public bool placeno { get; set; }
+    }
+
+}
