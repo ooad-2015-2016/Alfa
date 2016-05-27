057f82ac413bfaef80be838541789c6bb8579198
Semin Palalic <spalalic1@etf.unsa.ba>
2016-05-27 16:33:57 +0200

    Korisnik forma

--- Projekat/PogrebnoDrustvo/PogrebnoDrustvo/Korisnik2.xaml.cs
+++ Projekat/PogrebnoDrustvo/PogrebnoDrustvo/Korisnik2.xaml.cs
@@ -13,24 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace PogrebnoDrustvo
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Korisnik2 : Page
     {
-        public MainPage()
+        public Korisnik2()
         {
             this.InitializeComponent();
-        }
-
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            Korisnik2 f = new Korisnik2();
-            this.Content = f;
         }
     }
 }
